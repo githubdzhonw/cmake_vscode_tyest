0d7e7d9eadb88a8969a21b1d6fe2f0d1ecec228e
D Zheng <dz@example.com>
2023-06-04 00:06:41 -0700

    for c++20 features build

--- EP002/main.cpp
+++ EP002/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <concepts>
-
+using namespace std;
 template <typename T>
-requires std::integral<T>
-T add(T a, T b) {
+auto add(T a, T b) {
 	return a + b;
 }
 
 int main()
 {
 	std::cout << "hello c++ 20" << std::endl;
-	std::cout << "The sum is :" << add(7,5) << std::endl;
+	std::cout << "The sum is :" << add(7,3) << std::endl;
 	return 0;
 }
 
